107eee89bbb530a9e23e7d02f1e094f8aed1e9b7
Tom Shaw <tomshaw.work@gmail.com>
2024-10-05 15:19:49 +0100

    Update InventoryPane.java
    allow equipping two artifacts and two rings.

--- core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/InventoryPane.java
+++ core/src/main/java/com/shatteredpixel/shatteredpixeldungeon/ui/InventoryPane.java
@@ -307,6 +307,7 @@ public class InventoryPane extends Component {
 		equipped.get(2).item(stuff.artifact == null ? new WndBag.Placeholder( ItemSpriteSheet.ARTIFACT_HOLDER ) : stuff.artifact);
 		equipped.get(3).item(stuff.misc == null ? new WndBag.Placeholder( ItemSpriteSheet.SOMETHING ) : stuff.misc);
 		equipped.get(4).item(stuff.ring == null ? new WndBag.Placeholder( ItemSpriteSheet.RING_HOLDER ) : stuff.ring);
+		equipped.get(5).item(stuff.ring == null ? new WndBag.Placeholder( ItemSpriteSheet.RING_HOLDER ) : stuff.ring);
 
 		ArrayList<Item> items = (ArrayList<Item>) lastBag.items.clone();
 
